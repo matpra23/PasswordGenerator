dfb327d35248b204e2ab3b886525f30916799450
Mateusz Praski <138132651+matpra23@users.noreply.github.com>
2024-02-25 19:16:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,68 +1,15 @@
 package org.example;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static final Random random = new Random();
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         System.out.println("Input password length: ");
         int len = scanner.nextInt();
-        System.out.println("Your password: " + generatePassword(len));
+        System.out.println("Your password: " + Passwordgenerator.generatePassword(len));
 
-    }
-
-    public static String generatePassword(int len){
-        StringBuilder password = new StringBuilder();
-        String[] passwordArr = new String[len];
-        for (int i = 0; i < len; i++) {
-            int randomNum = random.nextInt(1, 4);
-            switch(randomNum){
-                case 1:
-                    passwordArr[i] = randomNumberGen();
-                    break;
-                case 2:
-                    passwordArr[i] = randomLetterGen();
-                    break;
-                case 3:
-                    passwordArr[i] = randomCharacterGen();
-                    break;
-                default:
-                    System.out.println("Random number generated is " + randomNum);
-                    break;
-            }
-            password.append(passwordArr[i]);
-        }
-        return password.toString();
-    }
-
-    static String randomNumberGen(){
-        String randomNumberGenerated = "";
-        String numbers = "0123456789";
-        int randN = random.nextInt(0,10);
-        char[] numbersArr = numbers.toCharArray();
-        randomNumberGenerated += numbersArr[randN];
-        return randomNumberGenerated;
-    }
-
-    static String randomLetterGen(){
-        String randomLetterGenerated = "";
-        String letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpRrSsTtUuWwYyZz";
-        int randN = random.nextInt(0, 46);
-        char[] letterArr = letters.toCharArray();
-        randomLetterGenerated += letterArr[randN];
-        return randomLetterGenerated;
-    }
-
-    static String randomCharacterGen(){
-        String randomCharGenerated = "";
-        String characters = "!@#$%^&*()_+-}{[]";
-        int  randomN = random.nextInt(0, 17);
-        char[] charactersArr = characters.toCharArray();
-        randomCharGenerated += charactersArr[randomN];
-        return randomCharGenerated;
     }
 }
